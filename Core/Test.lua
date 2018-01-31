5a5d197bb97325777f365316e4f7b9b229312844
Maxf <maxfqs@gmail.com>
2018-01-31 07:07:11 +0100

    Add Core/Test

---
+++ Core/Test.lua
@@ -0,0 +1,18 @@
+-- ===========================================================================
+-- file: Core/Test
+-- author: Hardballer
+--
+-- Test utility functions
+-- ===========================================================================
+
+local Event = require("Core/Event");
+local Test = {};
+
+
+--v function(cb: function(context: CA_UIContext?))
+function Test.onClick(cb) 
+    Event.addListener("ComponentLClickUp", cb);
+end
+
+
+return Test
