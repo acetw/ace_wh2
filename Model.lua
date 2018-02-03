6a6a54e4945984078aee4f8a8dd269286bef3830
maxfqs <maxfqs@gmail.com>
2018-02-03 13:47:29 +0100

    add Model

---
+++ Model.lua
@@ -0,0 +1,15 @@
+-- ===========================================================================
+-- file: Model
+-- author: Hardballer
+--
+-- Declare the Model namespace and class type
+-- The Model table is used as a mediator between all the model classes
+-- ===========================================================================
+
+--# assume global class ACE_Model
+
+
+local Model = {} --# assume Model: ACE_Model
+
+
+return Model
